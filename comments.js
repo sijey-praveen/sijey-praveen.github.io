0cf7073da389237d4eabec0a3ef19f1f57bce7a4
Sijey <cjpraveen@hotmail.com>
2021-11-24 10:53:54 +0530

    Update comments.js

--- comments.js
+++ comments.js
@@ -35,6 +35,5 @@ document.getElementById("post-comment").addEventListener("click",
         }
         var comment = {"username" : username, "text" : document.querySelector(".comment-text-feild").value, "avatar" : avatar}
         update_comments(comment)
-        console.log(document.cookie)
     }
 )
