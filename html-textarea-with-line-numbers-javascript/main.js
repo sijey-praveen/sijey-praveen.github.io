ffb9634d1ad8da1434ad18e12d56244f67a0bd95
Sijey <cjpraveen@hotmail.com>
2021-11-27 14:53:03 +0530

    Add files via upload

--- html-textarea-with-line-numbers-javascript/main.js
+++ html-textarea-with-line-numbers-javascript/main.js
@@ -3,18 +3,19 @@ const lines = document.querySelector(".lines")
 const main = document.getElementById("main")
 const events = ["change", "mousewheel", "scroll", "DOMContentLoaded", "keyup", "click"]
 
-for (let i = 0;i < events.length;i++) {
-    textarea.addEventListener(events[i],
-        function() {
-            let display = ""
-            for (let i = 0;i < textarea.value.split("\n").length;i++) {
-                display += `<span>${i}</span>`
-            }
-            lines.scrollTop = textarea.scrollTop
-            lines.innerHTML = display
-        }
-    )
+function render_lines() {
+    let display = ""
+    for (let i = 0;i < textarea.value.split("\n").length;i++) {
+        display += `<span>${i}</span>`
+    }
+    lines.scrollTop = textarea.scrollTop
+    lines.innerHTML = display
 }
 
+for (let i = 0;i < events.length;i++) {
+    textarea.addEventListener(events[i], () => {render_lines()})
+}
+
+document.addEventListener("DOMContentLoaded", () => {render_lines()})
 document.querySelector(".dark-btn").addEventListener("click", () => {main.className = "textfeild-dark"})
 document.querySelector(".light-btn").addEventListener("click", () => {main.className = "textfeild-light"})
