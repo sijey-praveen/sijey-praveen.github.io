13e5d46a5aa8476126c14b52b4f1e54b04b8be5f
Sijey <cjpraveen@hotmail.com>
2022-01-13 14:46:08 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,22 @@
 const variables = {
-    "typing" : document.querySelector(".typing")
+    "typing" : $(".typing").select(),
 }
 
 function __init__() {
-    new requests("GET", "https://github.com/chromium-projects/paperpro", null, true).response((data) => {
-        document.querySelector("#ppstarcount").innerText =  new BeautifulSoup(data, "#repo-stars-counter-star", "text").find
+    $().get("https://github.com/chromium-projects/paperpro", (data) => {
+        $("#ppstarcount").select().innerText = new BeautifulSoup(data, "#repo-stars-counter-star", "text").find
     })
 
-    new requests("GET", "https://github.com/sijey-praveen/YouTube-Music-API", null, true).response((data) => {
-        document.querySelector("#ytmstarcount").innerText = new BeautifulSoup(data, "#repo-stars-counter-star", "text").find
+    $().get("https://github.com/sijey-praveen/YouTube-Music-API", (data) => {
+        $("#ytmstarcount").select().innerText = new BeautifulSoup(data, "#repo-stars-counter-star", "text").find
     })
 
-    new requests("GET", "https://github.com/peregrine-lang/peregrine-lang.github.io", null, true).response((data) => {
-        document.querySelector("#plwstarcount").innerText = new BeautifulSoup(data, "#repo-stars-counter-star", "text").find
+    $().get("https://github.com/peregrine-lang/peregrine-lang.github.io", (data) => {
+        $("#plwstarcount").select().innerText = new BeautifulSoup(data, "#repo-stars-counter-star", "text").find
+    })
+
+    $().get("https://github.com/sijey-praveen/Pure.js", (data) => {
+        $("#pjsstarcount").select().innerText = new BeautifulSoup(data, "#repo-stars-counter-star", "text").find
     })
 
     setInterval(() => {
@@ -23,5 +27,5 @@ function __init__() {
     }, 1000)
 }
 
-document.addEventListener("DOMContentLoaded", __init__)
-window.onscroll = aos
+$().on("DOMContentLoaded", __init__)
+$("window").on("scroll", aos)
